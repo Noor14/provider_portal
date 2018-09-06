4a1357b82b178172274aa39027bf1beec2714016
Noor <nooraliyahoo95@gmail.com>
2018-09-06 15:17:03 +0500

    routing module implement

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'
+import { HeaderComponent } from './Shared/header/header.component';
+import { FooterComponent } from './Shared/footer/footer.component';
+import { RegistrationComponent } from './user/registration/registration.component';
+import { OtpconfirmationComponent } from './user/otpconfirmation/otpconfirmation.component';
+import { CreatePasswordComponent } from './user/create-password/create-password.component';
+
+const appRoutes:Routes = [
+  { path: 'registration', component : RegistrationComponent },
+  { path: '', redirectTo : 'registration', pathMatch: 'full' }
+]
+
+@NgModule({
+
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RouterModule, Routes } from '@angular/router'
-import { routing } from './app.routing';
 import { HeaderComponent } from './Shared/header/header.component';
 import { FooterComponent } from './Shared/footer/footer.component';
 import { RegistrationComponent } from './user/registration/registration.component';
-
-
+import { AgmCoreModule } from '@agm/core';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { OtpconfirmationComponent } from './user/otpconfirmation/otpconfirmation.component';
+import { CreatePasswordComponent } from './user/create-password/create-password.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    OtpconfirmationComponent,
+    CreatePasswordComponent,
   ],
   imports: [
     BrowserModule,
-    routing,
-    NgbModule.forRoot()
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgbModule.forRoot(),
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBs4tYXYGUA2kDvELgCYcbhYeoVgZCxumg&libraries=geometry',
+      libraries: ["places"]
+    })
 
   ],
   providers: [],
