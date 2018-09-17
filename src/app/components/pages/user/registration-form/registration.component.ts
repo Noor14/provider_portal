3eac4f929f59fc484a1c40ecfa30abfcccc862ef
Noor <nooraliyahoo95@gmail.com>
2018-09-17 19:55:15 +0500

    Translation api implemented

--- src/app/components/pages/user/registration-form/registration.component.ts
+++ src/app/components/pages/user/registration-form/registration.component.ts
@@ -7,6 +7,7 @@ import { Observable } from 'rxjs';
 import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
 import { UserService } from '../user.service';
 import { SharedService } from '../../../../services/shared.service';
+import { CommonService } from '../../../../services/common.service'
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
@@ -37,32 +38,68 @@ export class RegistrationComponent implements OnInit {
     lng: undefined
   }
 
+
+// model binding
+public transLangfirstName:any;
+public transLanglastName:any;
+public transLangjobTitle:any;
+public transLangEmail:any;
+
+
+// form Field Validtaions boolean variable
+public requiredFields: string = "This field is required";
+public firstNameError:boolean;
+public lastNameError:boolean;
+public contactError: boolean;
+public jobTitleError:boolean;
+public EmailError:boolean;
+public transfirstNameError:boolean;
+public translastNameError:boolean;
+public transjobTitleError:boolean;
+public transEmailError:boolean;
+
+
+
   constructor(
     private _userService: UserService,
+    private _commonService: CommonService,
     private _sharedService: SharedService,
     private mapsAPILoader: MapsAPILoader,
     private _router: Router,
     private ngZone: NgZone) { }
 
   ngOnInit() {
-
+    
+   
     this.regForm = new FormGroup({
-      firstName: new FormControl(null, { updateOn: 'blur', validators: [Validators.required, Validators.pattern(/^[a-zA-Z\s]*$/), Validators.minLength(2), Validators.maxLength(50)] }),
-      lastName: new FormControl(null, [Validators.required, Validators.pattern(/^[a-zA-Z\s]*$/), Validators.minLength(2), Validators.maxLength(50)]),
+      firstName: new FormControl(null, [Validators.required, Validators.pattern(/^[a-zA-Z\s]*$/), Validators.minLength(2), Validators.maxLength(100)]),
+      transLangfirstName: new FormControl(null, [Validators.required, Validators.pattern(/^[a-zA-Z\s]*$/), Validators.minLength(2), Validators.maxLength(100)]),
+      lastName: new FormControl(null, [Validators.required, Validators.pattern(/^[a-zA-Z\s]*$/), Validators.minLength(2), Validators.maxLength(100)]),
+      transLanglastName: new FormControl(null, [Validators.required, Validators.pattern(/^[a-zA-Z\s]*$/), Validators.minLength(2), Validators.maxLength(100)]),
       email: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/),
+        Validators.maxLength(320)
+      ]),
+      transLangEmail: new FormControl(null, [
         Validators.required,
         Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/),
         Validators.maxLength(320)
       ]),
       phone: new FormControl(null, [Validators.required, Validators.pattern(/^(?!(\d)\1+(?:\1+){0}$)\d+(\d+){0}$/), Validators.minLength(7), Validators.maxLength(12)]),
       jobTitle: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(25)]),
+      transLangjobTitle: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(25)]),
     });
 
 
     this._sharedService.countryList.subscribe((state: any) => {
       let List = state;
       List.map((obj) => {
+        if(typeof(obj.desc) == "string"){
         obj.desc = JSON.parse(obj.desc);
+      } else{
+        return
+      }
       })
       this.countryList = List;
     });
@@ -73,7 +110,7 @@ export class RegistrationComponent implements OnInit {
           title: state.country
         };
         let selectedCountry = this.countryList.find(obj => obj.title == state.country);
-        this.flag(selectedCountry);
+        this.selectPhoneCode(selectedCountry);
         this.getMapLatlng(obj);
       }
     })
@@ -96,8 +133,53 @@ export class RegistrationComponent implements OnInit {
         this.serviceOffered = res.returnObject;
       }
     })
+    
+    
+    // this.regForm.valueChanges.subscribe(val => {
+    //   this.regForm.controls.transLangfirstName.patchValue(val.firstName);
+    //   // val.transLangfirstName = val.firstName;
+    // });
 
   }
+  
+  errorValidate(){
+    if (this.regForm.controls.firstName.status == "INVALID" && this.regForm.controls.firstName.touched) {
+      this.firstNameError = true;
+    }
+    if (this.regForm.controls.transLangfirstName.status == "INVALID" && this.regForm.controls.transLangfirstName.touched) {
+      this.transfirstNameError = true;
+    }
+    if (this.regForm.controls.lastName.status == "INVALID" && this.regForm.controls.lastName.touched) {
+      this.lastNameError = true;
+    }
+    if (this.regForm.controls.transLanglastName.status == "INVALID" && this.regForm.controls.transLanglastName.touched) {
+      this.translastNameError = true;
+    }
+    if (this.regForm.controls.phone.status == "INVALID" && this.regForm.controls.phone.touched) {
+      this.contactError = true;
+    }
+    // if (this.regForm.controls.phone.status == "INVALID" && this.regForm.controls.phone.touched) {
+    //   this.contactError = true;
+    // }
+      // if (this.regForm.controls.phone.status == "INVALID" && this.regForm.controls.phone.touched) {
+    //   this.contactError = true;
+    // }
+    if (this.regForm.controls.email.status == "INVALID" && this.regForm.controls.email.touched) {
+      this.EmailError = true;
+    }
+    if (this.regForm.controls.transLangEmail.status == "INVALID" && this.regForm.controls.transLangEmail.touched) {
+      this.transEmailError = true;
+    }
+    
+    if (this.regForm.controls.jobTitle.status == "INVALID" && this.regForm.controls.jobTitle.touched) {
+      this.jobTitleError = true;
+    }
+    if (this.regForm.controls.transLangjobTitle.status == "INVALID" && this.regForm.controls.transLangjobTitle.touched) {
+      this.transjobTitleError = true;
+    }
+
+  }
+
   accountList(id) {
     this._userService.getAccountSetup(id).subscribe((res: any) => {
       if (res.returnStatus == 'Success') {
@@ -106,6 +188,7 @@ export class RegistrationComponent implements OnInit {
       }
     })
   }
+  
   getMapLatlng(region) {
     this._userService.getLatlng(region.title).subscribe((res: any) => {
       if (res.status == "OK") {
@@ -116,6 +199,7 @@ export class RegistrationComponent implements OnInit {
       }
     })
   }
+ 
   selectAccountSetup(id, obj){
     this.accountId = obj.AccountID;
     let elem = document.getElementsByClassName('fancyRadioBoxes') as any;
@@ -126,12 +210,14 @@ export class RegistrationComponent implements OnInit {
       
     }
   }
+ 
   NumberValid(evt) {
     let charCode = (evt.which) ? evt.which : evt.keyCode
     if (charCode > 31 && (charCode < 48 || charCode > 57))
       return false;
     return true;
   }
+  
   textValidation(event) {
     const pattern = /[a-zA-Z-][a-zA-Z -]*$/;
     let inputChar = String.fromCharCode(event.charCode);
@@ -139,7 +225,6 @@ export class RegistrationComponent implements OnInit {
       if (event.charCode == 0) {
         return true;
       }
-
       if (event.target.value) {
         var end = event.target.selectionEnd;
         if (event.keyCode == 32 && (event.target.value[end - 1] == " " || event.target.value[end] == " ")) {
@@ -154,10 +239,16 @@ export class RegistrationComponent implements OnInit {
     else {
       return true;
     }
-
+  }
+  
+  spaceHandler(event){
+    if (event.keyCode == 32) {
+      event.preventDefault();
+      return false;
+    }
   }
 
-  flag(list) {
+  selectPhoneCode(list) {
       this.countryFlagImage = list.code;
       let description = list.desc;
       this.phoneCode = description[0].CountryPhoneCode;
@@ -221,6 +312,17 @@ export class RegistrationComponent implements OnInit {
     })
 
   }
+
+  onModelChange($controlName, source, targeted, $value) {
+    setTimeout(()=>{
+      if(source && targeted){
+      this._commonService.translatedLanguage(source, targeted, $value).subscribe((res:any)=>{
+       this.regForm.controls[$controlName].patchValue(res.data.translations[0].translatedText);
+      })
+    }
+    },500)
+  }
+  
 
   search = (text$: Observable<string>) =>
     text$.pipe(
--- src/app/services/common.service.ts
+++ src/app/services/common.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { baseApi } from "../constants/base.url";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 
 @Injectable()
 export class CommonService {
@@ -10,6 +10,17 @@ export class CommonService {
     let url: string = "Country/GetDropDownDetail/0";
     return this._http.get(baseApi + url);
   }
+
+  translatedLanguage(sourceLanguage, targetLanguage, text) {
+    const params = new HttpParams()
+    .set('source', sourceLanguage)
+    .set('target', targetLanguage)
+    .set('q', text)
+    .set('key', 'AIzaSyDy8gRbCqDNl7BaN-rqW_r6IfMB45tf1oc');
+    let url: string = "https://translation.googleapis.com/language/translate/v2";
+    return this._http.get(url, {params});
+  }
+
    getBrowserlocation(){
     let url: string = 'http://ip-api.com/json';
     return this._http.get(url);
