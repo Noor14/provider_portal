1b3b4010f289231db5eb360967e54903471f411a
Noor <nooraliyahoo95@gmail.com>
2018-10-24 23:01:03 +0500

    all work merged hammad in noor branch

--- src/app/components/pages/user/user.guard.ts
+++ src/app/components/pages/user/user.guard.ts
@@ -9,7 +9,7 @@ import { SharedService } from '../../../services/shared.service';
 export class UserGuard implements CanActivate {
 
   public previousUrl;
-  constructor(private _userCreationService: UserCreationService, private router: Router, private _sharedService: SharedService) { 
+  constructor(private _userCreationService: UserCreationService, private router: Router, private _sharedService: SharedService) {
     // router.events
     //   .filter(event => event instanceof NavigationEnd)
     //   .subscribe(event => {
@@ -18,53 +18,26 @@ export class UserGuard implements CanActivate {
     //   });
   }
 
-  
-  canActivate(_route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean{
-   
-   
+  canActivate(_route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | boolean {
+
     // if user go to otp direct page
-    // return true;
-    if(state.url.indexOf('otp') >= 0){
+    if (state.url.indexOf('otp') >= 0) {
       let otpKey = state.url.split('/').pop();
-      if(otpKey != 'otp'){
-      return this._userCreationService.getUserInfoByOtp(otpKey).map((res: any) => {
-        if (res.returnStatus == "Success") {
-          this._sharedService.getUserInfoByOtp.next(res);
-          return true;
-        }
-        else{
-          this.router.navigate(['/registration']);
-          return true;
-        }
-      }, (err: HttpErrorResponse) => {
-        this.router.navigate(['/registration']);
-        return Observable.of(true);
-      })
-    }
+      if (otpKey != 'otp') {
+        return this.checkOtp(otpKey);
+      }
       else {
         this.router.navigate(['/registration']);
         return true;
       }
     }
-    
-    // if user go to password direct page
+
+    // // if user go to password direct page
 
     if (state.url.indexOf('password') >= 0) {
       let otpKey = state.url.split('/').pop();
       if (otpKey != 'password') {
-      return this._userCreationService.getUserOtpVerified(otpKey, 'Used').map((res: any) => {
-        if (res.returnStatus == "Success") {
-          this._sharedService.getUserOtpVerified.next(res);
-          return true;
-        }
-        else {
-          this.router.navigate(['/registration']);
-          return true;
-        }
-      }, (err: HttpErrorResponse) => {
-        this.router.navigate(['/registration']);
-        return Observable.of(true);
-      })
+        return this.checkPassword(otpKey);
       }
       else {
         this.router.navigate(['/registration']);
@@ -73,21 +46,20 @@ export class UserGuard implements CanActivate {
     }
 
     // if user go to direct business profile page or profile-completion page
-    if (state.url == '/business-profile' || state.url == '/profile-completion'){
-      if (localStorage.getItem('userInfo') && Object.keys('userInfo').length){
+    if (state.url == '/business-profile' || state.url == '/profile-completion') {
+      if (localStorage.getItem('userInfo') && Object.keys('userInfo').length) {
         return true;
       }
-      else{
+      else {
         this.router.navigate(['/registration']);
         return true;
-        
       }
     }
 
     // if user go to registration page
 
     if (state.url == '/registration') {
-      if (!localStorage.getItem('userInfo')) {
+      if (!localStorage.getItem('userInfo') || localStorage.getItem('userInfo')) {
         return true;
       }
       else {
@@ -98,4 +70,43 @@ export class UserGuard implements CanActivate {
     }
 
   }
+
+  checkOtp(otpKey): Observable<boolean> {
+    return this._userCreationService.getUserInfoByOtp(otpKey).map((res: any) => {
+      if (res.returnStatus == "Success") {
+        this._sharedService.getUserInfoByOtp.next(res);
+        return true;
+      }
+      else {
+        if (this.checkPassword(otpKey)) {
+          this.router.navigate(['/password', otpKey]);
+          return true;
+
+        } else {
+          this.router.navigate(['/registration']);
+          return true;
+        }
+      }
+    }, (err: HttpErrorResponse) => {
+      this.router.navigate(['/registration']);
+      return true;
+    })
+  }
+
+  checkPassword(otpKey): Observable<boolean> {
+    return this._userCreationService.getUserOtpVerified(otpKey, 'Used').map((res: any) => {
+      if (res.returnStatus == "Success") {
+        this._sharedService.getUserOtpVerified.next(res);
+        return true;
+      }
+      else {
+        this.router.navigate(['/registration']);
+        return true;
+      }
+    }, (err: HttpErrorResponse) => {
+      this.router.navigate(['/registration']);
+      return Observable.of(true);
+    })
+  }
+
 }
