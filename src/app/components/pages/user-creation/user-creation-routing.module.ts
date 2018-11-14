19ce89feb912186f48af7baa622a62e28be7d2ea
Noor <nooraliyahoo95@gmail.com>
2018-11-14 12:45:55 +0500

    renaming

--- src/app/components/pages/user-creation/user-creation-routing.module.ts
+++ src/app/components/pages/user-creation/user-creation-routing.module.ts
@@ -3,11 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { UserGuard } from './user.guard';
 import { RegistrationComponent } from './basic-info/registration-form/registration.component';
 import { OtpconfirmationComponent } from './basic-info/otpconfirmation/otpconfirmation.component';
-import { ProfilecompletionComponent } from './comapny-info/business-info/profilecompletion/profilecompletion.component';
+import { ProfilecompletionComponent } from './company-info/business-info/profilecompletion/profilecompletion.component';
 import { CreatePasswordComponent } from './basic-info/create-password/create-password.component';
 import { UserCreationComponent } from './user-creation.component';
-import { ShippingInfoComponent } from './comapny-info/shipping-info/shipping-info.component';
-import { BusinessInfoComponent } from './comapny-info/business-info/business-info.component';
+import { ShippingInfoComponent } from './company-info/shipping-info/shipping-info.component';
+import { BusinessInfoComponent } from './company-info/business-info/business-info.component';
 
 const routes: Routes = [
   {
