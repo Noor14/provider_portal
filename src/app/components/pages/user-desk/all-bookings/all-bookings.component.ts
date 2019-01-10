256be288628bd5b6fb59329752564450af5f284e
Hammad Bin Naeem <hammad@texpo.com>
2019-01-10 20:42:49 +0500

    All bookings tabs

--- src/app/components/pages/user-desk/all-bookings/all-bookings.component.ts
+++ src/app/components/pages/user-desk/all-bookings/all-bookings.component.ts
@@ -1,29 +1,30 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewEncapsulation } from '@angular/core';
 import { SharedService } from '../../../../services/shared.service';
 
 @Component({
   selector: 'app-all-bookings',
   templateUrl: './all-bookings.component.html',
-  styleUrls: ['./all-bookings.component.scss']
+  styleUrls: ['./all-bookings.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AllBookingsComponent implements OnInit, OnDestroy  {
+export class AllBookingsComponent implements OnInit, OnDestroy {
 
   public providerInfo;
   public allBookingsSubscriber;
   public bookings;
 
-  constructor(private _sharedService : SharedService) { }
+  constructor(private _sharedService: SharedService) { }
 
   ngOnInit() {
-    this.allBookingsSubscriber= this._sharedService.dashboardDetail.subscribe((state: any) => {
+    this.allBookingsSubscriber = this._sharedService.dashboardDetail.subscribe((state: any) => {
       if (state) {
-         this.providerInfo = state;
-         this.bookings = this.providerInfo.BookingDetails
+        this.providerInfo = state;
+        this.bookings = this.providerInfo.BookingDetails
       }
     });
   }
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.allBookingsSubscriber.unsubscribe();
-    }
+  }
 
 }
