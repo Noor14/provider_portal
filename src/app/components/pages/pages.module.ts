2e171a653f76be13a90d663f0ce8200cc645002f
Hammad Bin Naeem <hammad@texpo.com>
2019-01-11 20:25:58 +0500

    reports

--- src/app/components/pages/pages.module.ts
+++ src/app/components/pages/pages.module.ts
@@ -5,7 +5,6 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { PagesComponent } from './pages.component';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
-// import { LoaderComponent } from '../../shared/loader/loader.component'
 import { LoginDialogComponent } from '../../shared/dialogues/login-dialog/login-dialog.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ForgotPasswordComponent } from '../../shared/dialogues/forgot-password/forgot-password.component';
