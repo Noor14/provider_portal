7c3d6317c756c4d72a7bc6d8c0d88ba3e182503e
Noor <nooraliyahoo95@gmail.com>
2019-02-13 20:26:01 +0500

    rate history changes done

--- src/app/shared/dialogues/rate-history/rate-history.component.ts
+++ src/app/shared/dialogues/rate-history/rate-history.component.ts
@@ -112,8 +112,8 @@ export class RateHistoryComponent implements OnInit {
           })
           this.history = records[0].History;
         }
-        if (records[0].customer) {
-          let custDet = JSON.parse(records[0].customer)[0];
+        if (records[0].customer && records[0].customer.length) {
+          let custDet = records[0].customer[0];
           let obj = {
             CustomerImage: JSON.parse(custDet.CustomerImage)[0].DocumentFile,
             CustomerName: custDet.CustomerName
