fcfc1afdeadffa7dae292631dd9c5e0afd6ca292
Hashmove Developers <developers@hmdevmac01.local>
2019-02-15 20:27:23 +0500

    qa build for 7090

--- src/app/constants/base.url.ts
+++ src/app/constants/base.url.ts
@@ -4,30 +4,33 @@ export let baseApi;
 export let baseExternalAssets;
 if (environment.qa) {
     // QA URL
-    baseApi = "http://10.20.1.13:8091/api/";
-    baseExternalAssets = "http://10.20.1.13:8091";
-    
+    // baseApi = "http://10.20.1.13:8091/api/";
+    // baseExternalAssets = "http://10.20.1.13:8091";
+
+    baseApi = "http://10.20.1.13:7091/api/";
+    baseExternalAssets = "http://10.20.1.13:7091";
+
 }
 else if (environment.pers) {
     // DEV URL
     baseApi = "http://10.20.1.13:9091/api/";
     baseExternalAssets = "http://10.20.1.13:9091";
 
-} 
+}
 else if (environment.prod) {
     // PROD URL
     // baseApi = "http://partner.hashmove.com:81/api/";
     // baseExternalAssets = "http://partner.hashmove.com:81";
-    
+
     // baseApi = "https://betaapi.hashmove.com/api/";
     // baseExternalAssets = "https://betaapi.hashmove.com";
     baseApi = "https://betademoapi.hashmove.com/api/";
     baseExternalAssets = "https://betademoapi.hashmove.com";
 }
-else{
+else {
     // PERSONAL URL
     baseApi = "http://10.20.1.53/api/";
     baseExternalAssets = "http://10.20.1.53";
-} 
+}
 
 
