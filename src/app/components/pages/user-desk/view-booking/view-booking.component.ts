8709e857cb60e3672112709822334e7863536459
Noor <nooraliyahoo95@gmail.com>
2019-03-21 12:19:22 +0500

    booking status update

--- src/app/components/pages/user-desk/view-booking/view-booking.component.ts
+++ src/app/components/pages/user-desk/view-booking/view-booking.component.ts
@@ -231,7 +231,8 @@ export class ViewBookingComponent implements OnInit, OnDestroy {
     );
     modalRef.result.then((result) => {
       if (result.resType == "Success"){
-          this.bookingDetails.BookingStatus = result.status;
+        this.bookingDetails.BookingStatus = result.status;
+        this.bookingDetails.ShippingStatus = result.status;
       }
     });
     modalRef.componentInstance.modalData = {
