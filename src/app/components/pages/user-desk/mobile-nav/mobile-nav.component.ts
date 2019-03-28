67d6d65354eb36f1a901c0ce66aaf0983817a93f
Mubashira <mubashira@texpo.com>
2019-03-28 15:19:20 +0500

    atco card & upload-doc image ui changes

--- src/app/components/pages/user-desk/mobile-nav/mobile-nav.component.ts
+++ src/app/components/pages/user-desk/mobile-nav/mobile-nav.component.ts
@@ -9,33 +9,24 @@ import { baseExternalAssets } from '../../../../constants/base.url';
   templateUrl: './mobile-nav.component.html',
   styleUrls: ['./mobile-nav.component.scss']
 })
-// export class MobileNavComponent implements OnInit {
+export class MobileNavComponent implements OnInit {
 
 
-//   public showHide = false;
+  public showHide = false;
 
-//   constructor() {
+  constructor() {
 
-//    }
+   }
 
-//   ngOnInit() {
-    
-//   }
-
-
-
-//   clicked(){
-//     this.showHide = !this.showHide;  
-//   }
-
-// }
-
-
-export class MobileNavComponent  implements OnInit {
-  public baseExternalAssets: string = baseExternalAssets;
-  public statusCode:any = statusCode;
-  constructor(private _router: Router) { }
-  @Input() booking: any
   ngOnInit() {
+    
   }
+
+
+
+  clicked(){
+    this.showHide = !this.showHide;  
+  }
+
 }
+
